93c94cccf850b6514292c9ef3f9ded9cc7c4ccbb
Daniel Agapov <danielagapov1@gmail.com>
2021-12-13 22:08:31 -0500

    firebase config with web app instead of individual apps, also removing android/ios folders

---
+++ constants/firebase_config.tsx
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import secrets from "../secrets";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: secrets.apiKey,
+    authDomain: `{secrets.projectId}.firebaseapp.com`,
+    projectId: `{secrets.projectId}`,
+    storageBucket: `{secrets.projectId}.appspot.com`,
+    messagingSenderId: `{secrets.messagingSenderId}`,
+    appId: `{secrets.appId}`,
+    measurementId: `{secrets.measurementId}`,
+};
+
+// if (!firebase.apps.length) {
+//     firebase.initializeApp(firebaseConfig);
+// }
+
+// export { firebase };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
